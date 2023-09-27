e531562881db64388d2b31abf61c7b5bc95978cc
Delta <delta@deltawolf.net>
2023-09-27 05:54:26 -0700

    Formatting adjustments

--- ResoniteDirectEditGlobal/ResoniteDirectEditGlobal.cs
+++ ResoniteDirectEditGlobal/ResoniteDirectEditGlobal.cs
@@ -10,12 +10,12 @@ using System.Linq;
 
 namespace ResoniteDirectEditGlobal;
 public class ResoniteDirectEditGlobal : ResoniteMod {
-    public override string Name => "ResoniteDirectEditGlobal";
-    public override string Author => "Delta";
-    public override string Version => "1.2.0";
-    public override string Link => "https://github.com/XDelta/ResoniteDirectEditGlobal";
+	public override string Name => "ResoniteDirectEditGlobal";
+	public override string Author => "Delta";
+	public override string Version => "1.2.0";
+	public override string Link => "https://github.com/XDelta/ResoniteDirectEditGlobal";
 
-    static List<string> excludeNodesNames = new() { "GlobalTo", "ToGlobal" };
+	static List<string> excludeNodesNames = new() { "GlobalTo", "ToGlobal" };
 	public override void OnEngineInit() {
 		Harmony harmony = new Harmony("net.deltawolf.ResoniteDirectEditGlobal");
 		harmony.PatchAll();
@@ -25,70 +25,70 @@ public class ResoniteDirectEditGlobal : ResoniteMod {
 	class GenerateGlobalRefElement_Patch {
 		//Make sure the GlobalValue exists before calling the GenerateRefElement
 		public static void Prefix(ProtoFluxNodeVisual __instance, UIBuilder ui, Type referenceType, ISyncRef globalRef) {
-            //Any that would get a variable name should be edit-able
-            //any that would be a reference directly like an ibutton shouldn't be
+			//Any that would get a variable name should be edit-able
+			//any that would be a reference directly like an ibutton shouldn't be
 
-            //included:
-            //DynamicImpulses
-            //Dynamic Inputs
+			//included:
+			//DynamicImpulses
+			//Dynamic Inputs
 
-            //excluded by complex type: 
-            //Button Events
-            //excluded by name:
-            //GlobalToValueOutput
-            //WriteValueToGlobal
-            //GlobalToObjectOutput
+			//excluded by complex type: 
+			//Button Events
+			//excluded by name:
+			//GlobalToValueOutput
+			//WriteValueToGlobal
+			//GlobalToObjectOutput
 
-            if (!ShouldGenerate(__instance, referenceType, globalRef)) { return; }
+			if (!ShouldGenerate(__instance, referenceType)) { return; }
 
-            var globalValue = globalRef.Target ?? __instance.Node.Target.Slot.GetComponent<GlobalValue<string>>();
-            //If for whatever reason it isn't set and it already has a globalvalue, this should reconnect it
+			var globalValue = globalRef.Target ?? __instance.Node.Target.Slot.GetComponent<GlobalValue<string>>();
+			//If for whatever reason it isn't set and it already has a globalvalue, this should reconnect it
 			if (globalValue == null) {
-                Debug("Couldn't find GlobalValue, adding one");
+				Debug("Couldn't find GlobalValue, adding one");
 				globalValue = __instance.Node.Target.Slot.AttachComponent<GlobalValue<string>>();
 			}
 			globalRef.Target = globalValue;
-        }
+		}
 
-        public static void Postfix(ProtoFluxNodeVisual __instance, UIBuilder ui, Type referenceType, ISyncRef globalRef) {
+		public static void Postfix(ProtoFluxNodeVisual __instance, UIBuilder ui, Type referenceType, ISyncRef globalRef) {
 
-            if (!ShouldGenerate(__instance, referenceType, globalRef)) { return; }
+			if (!ShouldGenerate(__instance, referenceType)) { return; }
 			if (globalRef == null) {
 				Debug("GlobalRef was null");
 				return;
 			}
 
-            var btn = ui.Current.FindChildInHierarchy("Horizontal Layout")[0];
+			var btn = ui.Current.FindChildInHierarchy("Horizontal Layout")[0];
 			var btntext = btn[0].GetComponent<Text>();
-            btntext.Content.ReleaseLink(btntext.Content.ActiveLink);
-            var pme = btn.AttachComponent<PrimitiveMemberEditor>();
+			btntext.Content.ReleaseLink(btntext.Content.ActiveLink);
+			var pme = btn.AttachComponent<PrimitiveMemberEditor>();
 			var tf = btn.AttachComponent<TextField>();
-            var te = btn.GetComponentOrAttach<TextEditor>();
+			var te = btn.GetComponentOrAttach<TextEditor>();
 
-            te.Text.Target = btntext;
+			te.Text.Target = btntext;
 			var _te = (SyncRef<TextEditor>)pme.TryGetField("_textEditor");
 			_te.Target = tf.Editor.Target;
-            var _td = (FieldDrive<string>)pme.TryGetField("_textDrive");
+			var _td = (FieldDrive<string>)pme.TryGetField("_textDrive");
 			_td.Target = btntext.Content;
-            var _target = (RelayRef<IField>)pme.TryGetField("_target");
+			var _target = (RelayRef<IField>)pme.TryGetField("_target");
 			_target.Target = __instance.Node.Target.Slot.GetComponent<GlobalValue<string>>().Value;
 		}
 
-        internal static bool ShouldGenerate(ProtoFluxNodeVisual instance, Type referenceType, ISyncRef globalRef) {
-            if (referenceType != typeof(string)) {
-                if (Coder.IsEnginePrimitive(referenceType)) {
-                    Debug($"Valid non-string primitive: {referenceType}"); //simpler types like bool, int, string, float
-                } else {
-                    Debug($"Not valid primitive: {referenceType}, skipping generation"); //complex types like IButton
-                    return false;
-                }
-            }
+		internal static bool ShouldGenerate(ProtoFluxNodeVisual instance, Type referenceType) {
+			if (referenceType != typeof(string)) {
+				if (Coder.IsEnginePrimitive(referenceType)) {
+					Debug($"Valid non-string primitive: {referenceType}"); //simpler types like bool, int, string, float
+				} else {
+					Debug($"Not valid primitive: {referenceType}, skipping generation"); //complex types like IButton
+					return false;
+				}
+			}
 
-            if (excludeNodesNames.Any(instance.Node.Target.Name.Contains)) {
-                Debug($"Excluding {instance.Node.Target.Name}");
-                return false; 
-            }
-            return true;
-        }
+			if (excludeNodesNames.Any(instance.Node.Target.Name.Contains)) {
+				Debug($"Excluding {instance.Node.Target.Name}");
+				return false;
+			}
+			return true;
+		}
 	}
 }
